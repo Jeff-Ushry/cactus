832b83528e71d1b43a22f6ba3619bc109a7bd770
github-action-benchmark <github@users.noreply.github.com>
2024-05-19 16:37:07 +0000

    add Benchmark (benchmarkjs) benchmark result for eacaef3cc52512b74b11cdc1b8c1b09e4d82460e

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716136627306,
+  "repoUrl": "https://github.com/Jeff-Ushry/cactus",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zondervan.v.calvez@accenture.com",
+            "name": "zondervancalvez",
+            "username": "zondervancalvez"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "eacaef3cc52512b74b11cdc1b8c1b09e4d82460e",
+          "message": "chore(github): shorten ci.yaml workflow and job names\n\nce - cactus-example\ncp - cactus-plugin\ncpk - cactus-plugin-keychain\ncpl - cactus-plugin-ledger\ncplc - cactus-plugin-ledger-connector\nplc - plugin-ledger-connector\ncpp - cactus-plugin-persistence\nct - cactus-test\nctp - cactus-test-plugin\n\nFixes: hyperledger#2624\n\nSigned-off-by: zondervancalvez <zondervan.v.calvez@accenture.com>",
+          "timestamp": "2024-05-18T16:17:18-07:00",
+          "tree_id": "36312b30100d2d6e1069e72c17470d211eadc167",
+          "url": "https://github.com/Jeff-Ushry/cactus/commit/eacaef3cc52512b74b11cdc1b8c1b09e4d82460e"
+        },
+        "date": 1716136624828,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 496,
+            "range": "±1.97%",
+            "unit": "ops/sec",
+            "extra": "175 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 310,
+            "range": "±1.53%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
