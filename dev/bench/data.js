e5124c253e197e210df2c333d78936f684fb4383
github-action-benchmark <github@users.noreply.github.com>
2024-05-19 16:52:05 +0000

    add Benchmark (benchmarkjs) benchmark result for eacaef3cc52512b74b11cdc1b8c1b09e4d82460e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716136627306,
+  "lastUpdate": 1716137525227,
   "repoUrl": "https://github.com/Jeff-Ushry/cactus",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.53%",
             "unit": "ops/sec",
             "extra": "179 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zondervan.v.calvez@accenture.com",
+            "name": "zondervancalvez",
+            "username": "zondervancalvez"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "eacaef3cc52512b74b11cdc1b8c1b09e4d82460e",
+          "message": "chore(github): shorten ci.yaml workflow and job names\n\nce - cactus-example\ncp - cactus-plugin\ncpk - cactus-plugin-keychain\ncpl - cactus-plugin-ledger\ncplc - cactus-plugin-ledger-connector\nplc - plugin-ledger-connector\ncpp - cactus-plugin-persistence\nct - cactus-test\nctp - cactus-test-plugin\n\nFixes: hyperledger#2624\n\nSigned-off-by: zondervancalvez <zondervan.v.calvez@accenture.com>",
+          "timestamp": "2024-05-18T16:17:18-07:00",
+          "tree_id": "36312b30100d2d6e1069e72c17470d211eadc167",
+          "url": "https://github.com/Jeff-Ushry/cactus/commit/eacaef3cc52512b74b11cdc1b8c1b09e4d82460e"
+        },
+        "date": 1716137524019,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 763,
+            "range": "±3.01%",
+            "unit": "ops/sec",
+            "extra": "180 samples"
           }
         ]
       }
